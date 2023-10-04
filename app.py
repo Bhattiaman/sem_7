b440745f55de5621bdb21d8e54e833f8883cd3ff
Bhattiaman <bhattiaman@kali.Bhattiaman>
2023-10-04 10:59:17 +0530

    update

--- app.py
+++ app.py
@@ -12,14 +12,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html',
-                           book_name = list(popular_df['Book-Title'].values),
+                           book_name = list(popular_df['Book-Title'].values), 
                            author=list(popular_df['Book-Author'].values),
                            image=list(popular_df['Image-URL-M'].values),
                            votes=list(popular_df['num_ratings'].values),
                            rating=list(popular_df['avg_rating'].values)
                            )
 
+
+
+
 @app.route('/recommend')
+
+
 def recommend_ui():
     return render_template('recommend.html')
 
@@ -42,6 +47,11 @@ def recommend():
     print(data)
 
     return render_template('recommend.html',data=data)
+
+@app.route('/audio.html')
+def audio():
+    return render_template('audio.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
