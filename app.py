faf03d35edd67f6b96c995b26ddcb83db770f644
Bhattiaman <bhattiaman@kali.Bhattiaman>
2023-10-04 22:11:53 +0530

    audio

--- app.py
+++ app.py
@@ -1,34 +1,29 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-popular_df = pickle.load(open('populars.pkl','rb'))
-pt = pickle.load(open('pt.pkl','rb'))
-books = pickle.load(open('books.pkl','rb'))
-similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+popular_df = pickle.load(open('populars.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html',
-                           book_name = list(popular_df['Book-Title'].values), 
+                           book_name=list(popular_df['Book-Title'].values),
                            author=list(popular_df['Book-Author'].values),
                            image=list(popular_df['Image-URL-M'].values),
                            votes=list(popular_df['num_ratings'].values),
                            rating=list(popular_df['avg_rating'].values)
                            )
 
-
-
-
 @app.route('/recommend')
-
-
 def recommend_ui():
     return render_template('recommend.html')
 
-@app.route('/recommend_books',methods=['post'])
+@app.route('/recommend_books', methods=['POST'])
 def recommend():
     user_input = request.form.get('user_input')
     index = np.where(pt.index == user_input)[0][0]
@@ -46,12 +41,27 @@ def recommend():
 
     print(data)
 
-    return render_template('recommend.html',data=data)
+    return render_template('recommend.html', data=data)
 
 @app.route('/audio.html')
 def audio():
     return render_template('audio.html')
 
+@app.route('/style.css')
+def style_css():
+    return render_template('style.css')
+
+@app.route('/script.js')
+def script_js():
+    return render_template('script.js')
+
+@app.route('/songs')
+def songs():
+    return render_template('songs.html')
+
+@app.route('/covers')
+def covers():
+    return render_template('covers.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
